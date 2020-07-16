7e8bd548a0283621120b80d1baab2d2c0a398916
sam leider <sam.leider@hotmail.com>
2020-07-15 21:05:16 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+/* initalize port variable */
+const PORT = process.env.PORT || 3050
+
+app.get('/hello', (req, res) => res.send({ hi: 'there' }))
+// initalize static routes in production
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware')
+  const webpack = require('webpack')
+  const webpackConfig = require('./webpack.config.js')
+  app.use(webpackMiddleware(webpack(webpackConfig)))
+} else {
+  app.use(express.static('dist'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  })
+}
+
+// Start server
+app.listen(PORT, () => {
+  console.log('Server started on port ' + PORT)
+})
